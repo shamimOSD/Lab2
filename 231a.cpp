128dd10da20f090cd8041da9c3f80a5a23ce7f20
shamimOSD <141517665+shamimOSD@users.noreply.github.com>
2023-08-06 13:34:27 +0600

    Add files via upload

---
+++ 231a.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+   int n,p,v,t,s=0;
+   cin>>n;
+   for(int i=0;i<n;i++)
+   {
+      cin>>p>>v>>t;
+      if((p==1&&v==1)||(v==1&&t==1)||(t==1&&p==1))
+         s++;
+
+   }
+   cout<<s<<endl;
+   return 0;
+}
